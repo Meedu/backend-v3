83ce26ded909ca52b78be2bf38db7234d1284a6d
禺狨 <18119604035@163.com>
2023-05-18 15:15:58 +0800

    积分商城-添加商品页面除介绍富文本组件封装

--- src/pages/creditMall/create.tsx
+++ src/pages/creditMall/create.tsx
@@ -48,7 +48,7 @@ const CreditMallCreatePage = () => {
   useEffect(() => {
     document.title = "添加商品";
     dispatch(titleAction("添加商品"));
-    form.setFieldsValue({ is_show: 1 });
+    form.setFieldsValue({ is_show: 1, is_v: 0 });
     getParams();
   }, []);
 
@@ -156,127 +156,123 @@ const CreditMallCreatePage = () => {
           onFinishFailed={onFinishFailed}
           autoComplete="off"
         >
-          {resourceActive === "base" && (
-            <>
-              <Form.Item
-                label="是否虚拟商品"
-                name="is_v"
-                valuePropName="checked"
-              >
-                <Switch onChange={isVChange} />
+          <div
+            style={{ display: resourceActive === "base" ? "block" : "none" }}
+          >
+            <Form.Item label="是否虚拟商品" name="is_v" valuePropName="checked">
+              <Switch onChange={isVChange} />
+            </Form.Item>
+            {is_v === 1 && (
+              <Form.Item name="v_type" label="虚拟商品类型">
+                <Select
+                  style={{ width: 300 }}
+                  allowClear
+                  placeholder="请选择虚拟商品类型"
+                  options={goodsTypes}
+                  onChange={(e) => {
+                    setType(e);
+                  }}
+                />
               </Form.Item>
-              {is_v === 1 && (
-                <Form.Item name="v_type" label="虚拟商品类型">
-                  <Select
-                    style={{ width: 300 }}
-                    allowClear
-                    placeholder="请选择虚拟商品类型"
-                    options={goodsTypes}
-                    onChange={(e) => {
-                      setType(e);
+            )}
+            {is_v === 1 && type && (
+              <Form.Item label="虚拟商品" name="v_id">
+                <Button
+                  loading={loading}
+                  type="primary"
+                  onClick={() => setShowSelectResWin(true)}
+                >
+                  {title && <span>已选择「{title}」</span>}
+                  {!title && <span>选择商品</span>}
+                </Button>
+              </Form.Item>
+            )}
+            <Form.Item
+              label="商品名"
+              name="title"
+              rules={[{ required: true, message: "请输入商品名!" }]}
+            >
+              <Input
+                style={{ width: 300 }}
+                placeholder="请输入商品名"
+                allowClear
+              />
+            </Form.Item>
+            <Form.Item
+              label="商品封面"
+              name="thumb"
+              rules={[{ required: true, message: "请上传商品封面!" }]}
+            >
+              <UploadImageButton
+                text="上传封面"
+                onSelected={(url) => {
+                  form.setFieldsValue({ thumb: url });
+                  setThumb(url);
+                }}
+              ></UploadImageButton>
+            </Form.Item>
+            {thumb && (
+              <Row style={{ marginBottom: 22 }}>
+                <Col span={3}></Col>
+                <Col span={21}>
+                  <div
+                    className="contain-thumb-box"
+                    style={{
+                      backgroundImage: `url(${thumb})`,
+                      width: 400,
+                      height: 400,
                     }}
+                  ></div>
+                </Col>
+              </Row>
+            )}
+            <Form.Item
+              label="价格"
+              name="charge"
+              rules={[{ required: true, message: "请输入价格!" }]}
+            >
+              <Space align="baseline" style={{ height: 32 }}>
+                <Form.Item
+                  name="charge"
+                  rules={[{ required: true, message: "请输入价格!" }]}
+                >
+                  <Input
+                    style={{ width: 300 }}
+                    placeholder="单位：积分"
+                    allowClear
+                    type="number"
                   />
                 </Form.Item>
-              )}
-              {is_v === 1 && type && (
-                <Form.Item label="虚拟商品" name="v_id">
-                  <Button
-                    loading={loading}
-                    type="primary"
-                    onClick={() => setShowSelectResWin(true)}
-                  >
-                    {title && <span>已选择「{title}」</span>}
-                    {!title && <span>选择商品</span>}
-                  </Button>
-                </Form.Item>
-              )}
-              <Form.Item
-                label="商品名"
-                name="title"
-                rules={[{ required: true, message: "请输入商品名!" }]}
-              >
-                <Input
-                  style={{ width: 300 }}
-                  placeholder="请输入商品名"
-                  allowClear
-                />
-              </Form.Item>
-              <Form.Item
-                label="商品封面"
-                name="thumb"
-                rules={[{ required: true, message: "请上传商品封面!" }]}
-              >
-                <UploadImageButton
-                  text="上传封面"
-                  onSelected={(url) => {
-                    form.setFieldsValue({ thumb: url });
-                    setThumb(url);
-                  }}
-                ></UploadImageButton>
-              </Form.Item>
-              {thumb && (
-                <Row style={{ marginBottom: 22 }}>
-                  <Col span={3}></Col>
-                  <Col span={21}>
-                    <div
-                      className="contain-thumb-box"
-                      style={{
-                        backgroundImage: `url(${thumb})`,
-                        width: 400,
-                        height: 400,
-                      }}
-                    ></div>
-                  </Col>
-                </Row>
-              )}
-              <Form.Item
-                label="价格"
-                name="charge"
-                rules={[{ required: true, message: "请输入价格!" }]}
-              >
-                <Space align="baseline" style={{ height: 32 }}>
-                  <Form.Item
-                    name="charge"
-                    rules={[{ required: true, message: "请输入价格!" }]}
-                  >
-                    <Input
-                      style={{ width: 300 }}
-                      placeholder="单位：积分"
-                      allowClear
-                      type="number"
-                    />
-                  </Form.Item>
-                  <div className="ml-10">
-                    <HelperText text="价格最小单位为：积分，不支持小数"></HelperText>
-                  </div>
-                </Space>
-              </Form.Item>
-              <Form.Item
-                label="介绍"
-                name="desc"
-                rules={[{ required: true, message: "请输入介绍!" }]}
-              >
-                <Input
-                  style={{ width: 300 }}
-                  placeholder="请输入内容"
-                  allowClear
-                />
-              </Form.Item>
-              <Form.Item
-                label="库存"
-                name="stock_count"
-                rules={[{ required: true, message: "请输入库存!" }]}
-              >
-                <Input
-                  type="number"
-                  style={{ width: 300 }}
-                  placeholder="商品库存数量"
-                  allowClear
-                />
-              </Form.Item>
-            </>
-          )}
-          {resourceActive === "dev" && (
+                <div className="ml-10">
+                  <HelperText text="价格最小单位为：积分，不支持小数"></HelperText>
+                </div>
+              </Space>
+            </Form.Item>
+            <Form.Item
+              label="介绍"
+              name="desc"
+              rules={[{ required: true, message: "请输入介绍!" }]}
+            >
+              <Input
+                style={{ width: 300 }}
+                placeholder="请输入内容"
+                allowClear
+              />
+            </Form.Item>
+            <Form.Item
+              label="库存"
+              name="stock_count"
+              rules={[{ required: true, message: "请输入库存!" }]}
+            >
+              <Input
+                type="number"
+                style={{ width: 300 }}
+                placeholder="商品库存数量"
+                allowClear
+              />
+            </Form.Item>
+          </div>
+          <div style={{ display: resourceActive === "dev" ? "block" : "none" }}>
             <Form.Item label="显示" name="is_show">
               <Space align="baseline" style={{ height: 32 }}>
                 <Form.Item name="is_show" valuePropName="checked">
@@ -287,7 +283,7 @@ const CreditMallCreatePage = () => {
                 </div>
               </Space>
             </Form.Item>
-          )}
+          </div>
         </Form>
       </div>
       <div className="bottom-menus">

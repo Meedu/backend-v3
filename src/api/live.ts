7fcd57f4450075e397eab2d530d1ab6052f6b878
禺狨 <18119604035@163.com>
2023-05-31 17:29:17 +0800

    课程-直播课列表

--- src/api/live.ts
+++ src/api/live.ts
@@ -3,3 +3,23 @@ import client from "./internal/httpClient";
 export function list(params: any) {
   return client.get(`/backend/addons/zhibo/course/index`, params);
 }
+
+export function create() {
+  return client.get(`/backend/addons/zhibo/course/create`, {});
+}
+
+export function store(params: any) {
+  return client.post("/backend/addons/zhibo/course/create", params);
+}
+
+export function detail(id: number) {
+  return client.get(`/backend/addons/zhibo/course/${id}`, {});
+}
+
+export function destroy(id: number) {
+  return client.destroy(`/backend/addons/zhibo/course/${id}`);
+}
+
+export function update(id: number, params: any) {
+  return client.put(`/backend/addons/zhibo/course/${id}`, params);
+}
--- src/pages/live/index.tsx
+++ src/pages/live/index.tsx
@@ -57,6 +57,19 @@ const LivePage = () => {
     getData();
   }, [page, size, refresh]);
 
+  useEffect(() => {
+    if (
+      (category_id && category_id.length !== 0) ||
+      (teacher_id && teacher_id.length !== 0) ||
+      status !== -1 ||
+      keywords
+    ) {
+      setShowStatus(true);
+    } else {
+      setShowStatus(false);
+    }
+  }, [category_id, teacher_id, status, keywords]);
+
   const getData = () => {
     if (loading) {
       return;
@@ -112,7 +125,7 @@ const LivePage = () => {
         let teachers = res.data.teachers;
         const box3: any = [];
         for (let i = 0; i < teachers.length; i++) {
-          box2.push({
+          box3.push({
             label: teachers[i].name,
             value: teachers[i].id,
           });
@@ -148,6 +161,210 @@ const LivePage = () => {
   const handlePageChange = (page: number, pageSize: number) => {
     setPage(page);
     setSize(pageSize);
+  };
+
+  const columns: ColumnsType<DataType> = [
+    {
+      title: "ID",
+      width: "6%",
+      render: (_, record: any) => <span>{record.id}</span>,
+    },
+    {
+      title: "直播课程",
+      width: "22%",
+      render: (_, record: any) => (
+        <ThumbBar
+          width={120}
+          value={record.thumb}
+          height={90}
+          title={record.title}
+          border={4}
+        ></ThumbBar>
+      ),
+    },
+    {
+      title: "分类",
+      width: "8%",
+      render: (_, record: any) => (
+        <>
+          {record.category && <span>{record.category.name}</span>}
+          {!record.category && <span className="c-red">数据不完整</span>}
+        </>
+      ),
+    },
+    {
+      title: "讲师/助教",
+      width: "8%",
+      render: (_, record: any) => (
+        <>
+          <span>{record.teacher.name}</span>
+          {record.assistant && <span>/{record.assistant.name}</span>}
+        </>
+      ),
+    },
+    {
+      title: "价格",
+      width: "8%",
+      render: (_, record: any) => <span>{record.charge}元</span>,
+    },
+    {
+      title: "销量",
+      width: "8%",
+      render: (_, record: any) => <span>{record.join_user_times}</span>,
+    },
+    {
+      title: "下一场直播时间",
+      width: "18%",
+      render: (_, record: any) => (
+        <>
+          {record.status === 2 && (
+            <span className="c-gray">· {record.status_text}</span>
+          )}
+          {record.status !== 2 && (
+            <>
+              {record.next_video.length === 0 ? (
+                <span>-</span>
+              ) : (
+                <span>{dateFormat(record.published_at)}</span>
+              )}
+            </>
+          )}
+        </>
+      ),
+    },
+    {
+      title: "是否显示",
+      width: "8%",
+      render: (_, record: any) => (
+        <>
+          {record.is_show === 1 && <span className="c-green">· 显示</span>}
+          {record.is_show !== 1 && <span className="c-red">· 隐藏</span>}
+        </>
+      ),
+    },
+    {
+      title: "操作",
+      width: "14%",
+      fixed: "right",
+      render: (_, record: any) => {
+        const items: MenuProps["items"] = [
+          {
+            key: "1",
+            label: (
+              <PerButton
+                type="link"
+                text="编辑"
+                class="c-primary"
+                icon={null}
+                p="addons.Zhibo.course.update"
+                onClick={() => {
+                  navigate("/live/course/update?id=" + record.id);
+                }}
+                disabled={null}
+              />
+            ),
+          },
+          {
+            key: "2",
+            label: (
+              <PerButton
+                type="link"
+                text="删除"
+                class="c-red"
+                icon={null}
+                p="addons.Zhibo.course.delete"
+                onClick={() => {
+                  destory(record.id);
+                }}
+                disabled={null}
+              />
+            ),
+          },
+        ];
+        return (
+          <Space>
+            <PerButton
+              type="link"
+              text="排课"
+              class="c-primary"
+              icon={null}
+              p="addons.Zhibo.course_video.list"
+              onClick={() => {
+                navigate(
+                  "/live/course/video/index?id=" +
+                    record.id +
+                    "&title=" +
+                    record.title
+                );
+              }}
+              disabled={null}
+            />
+            <PerButton
+              type="link"
+              text="学员"
+              class="c-primary"
+              icon={null}
+              p="addons.Zhibo.course.users"
+              onClick={() => {
+                navigate("/live/course/users/index?id=" + record.id);
+              }}
+              disabled={null}
+            />
+            <Dropdown menu={{ items }}>
+              <Button
+                type="link"
+                className="c-primary"
+                onClick={(e) => e.preventDefault()}
+              >
+                <Space size="small" align="center">
+                  更多
+                  <DownOutlined />
+                </Space>
+              </Button>
+            </Dropdown>
+          </Space>
+        );
+      },
+    },
+  ];
+
+  const resetData = () => {
+    setPage(1);
+    setList([]);
+    setRefresh(!refresh);
+  };
+
+  const destory = (id: number) => {
+    if (id === 0) {
+      return;
+    }
+    confirm({
+      title: "操作确认",
+      icon: <ExclamationCircleFilled />,
+      content: "确认删除此课程？",
+      centered: true,
+      okText: "确认",
+      cancelText: "取消",
+      onOk() {
+        if (loading) {
+          return;
+        }
+        setLoading(true);
+        live
+          .destroy(id)
+          .then(() => {
+            setLoading(false);
+            message.success("删除成功");
+            resetData();
+          })
+          .catch((e) => {
+            setLoading(false);
+          });
+      },
+      onCancel() {
+        console.log("Cancel");
+      },
+    });
   };
 
   return (
@@ -238,6 +455,85 @@ const LivePage = () => {
           </div>
         </div>
       </div>
+      <div className="float-left">
+        <Table
+          loading={loading}
+          columns={columns}
+          dataSource={list}
+          rowKey={(record) => record.id}
+          pagination={paginationProps}
+        />
+      </div>
+      <Drawer
+        title="更多筛选"
+        onClose={() => setDrawer(false)}
+        maskClosable={false}
+        open={drawer}
+        footer={
+          <Space className="j-b-flex">
+            <Button
+              onClick={() => {
+                resetList();
+                setDrawer(false);
+              }}
+            >
+              清空
+            </Button>
+            <Button
+              onClick={() => {
+                setPage(1);
+                setRefresh(!refresh);
+                setDrawer(false);
+              }}
+              type="primary"
+            >
+              筛选
+            </Button>
+          </Space>
+        }
+        width={360}
+      >
+        <div className="float-left">
+          <Input
+            value={keywords}
+            onChange={(e) => {
+              setKeywords(e.target.value);
+            }}
+            allowClear
+            placeholder="课程名称关键字"
+          />
+          <Select
+            style={{ width: "100%", marginTop: 20 }}
+            value={category_id}
+            onChange={(e) => {
+              setCategoryId(e);
+            }}
+            allowClear
+            placeholder="分类"
+            options={categories}
+          />
+          <Select
+            style={{ width: "100%", marginTop: 20 }}
+            value={teacher_id}
+            onChange={(e) => {
+              setTeacherId(e);
+            }}
+            allowClear
+            placeholder="讲师"
+            options={teachers}
+          />
+          <Select
+            style={{ width: "100%", marginTop: 20 }}
+            value={status}
+            onChange={(e) => {
+              setStatus(e);
+            }}
+            allowClear
+            placeholder="状态"
+            options={statusList}
+          />
+        </div>
+      </Drawer>
     </div>
   );
 };

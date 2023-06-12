dcf6ef9d153382d5a5ab9e31f50c30fd754e951b
禺狨 <18119604035@163.com>
2023-06-12 14:57:00 +0800

    init初始化优化

--- src/pages/init/index.tsx
+++ src/pages/init/index.tsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { Outlet } from "react-router-dom";
 import { loginAction } from "../../store/user/loginUserSlice";
@@ -16,42 +17,51 @@ interface Props {
 
 const InitPage = (props: Props) => {
   const dispatch = useDispatch();
-  if (props.loginData) {
-    dispatch(loginAction(props.loginData));
-  }
+  const [init, setInit] = useState<boolean>(false);
 
-  if (props.configData) {
-    let config: SystemConfigStoreInterface = {
-      system: {
-        logo: props.configData.system.logo,
-        url: {
-          api: props.configData.system.url.api,
-          h5: props.configData.system.url.h5,
-          pc: props.configData.system.url.pc,
-        },
-      },
-      video: {
-        default_service: props.configData.video.default_service,
-      },
-    };
-    dispatch(saveConfigAction(config));
-  }
-
-  if (props.addonsData) {
-    let enabledAddons: any = {};
-    let count = 0;
-    for (let i = 0; i < props.addonsData.length; i++) {
-      if (props.addonsData[i].enabled) {
-        count += 1;
-        enabledAddons[props.addonsData[i].sign] = 1;
-      }
+  useEffect(() => {
+    if (props.loginData) {
+      dispatch(loginAction(props.loginData));
     }
-    dispatch(setEnabledAddonsAction({ addons: enabledAddons, count: count }));
-  }
+
+    if (props.configData) {
+      let config: SystemConfigStoreInterface = {
+        system: {
+          logo: props.configData.system.logo,
+          url: {
+            api: props.configData.system.url.api,
+            h5: props.configData.system.url.h5,
+            pc: props.configData.system.url.pc,
+          },
+        },
+        video: {
+          default_service: props.configData.video.default_service,
+        },
+      };
+      dispatch(saveConfigAction(config));
+    }
+
+    if (props.addonsData) {
+      let enabledAddons: any = {};
+      let count = 0;
+      for (let i = 0; i < props.addonsData.length; i++) {
+        if (props.addonsData[i].enabled) {
+          count += 1;
+          enabledAddons[props.addonsData[i].sign] = 1;
+        }
+      }
+      dispatch(setEnabledAddonsAction({ addons: enabledAddons, count: count }));
+    }
+    setInit(true);
+  }, [props]);
 
   return (
     <>
-      <Outlet />
+      {init && (
+        <div>
+          <Outlet />
+        </div>
+      )}
     </>
   );
 };

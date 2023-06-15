69d7d38ee07c1e3d41a818b803e36adb2546a04c
禺狨 <18119604035@163.com>
2023-06-15 20:33:03 +0800

    pc装修电子书配置组件

--- src/pages/decoration/components/h5/config/index.tsx
+++ src/pages/decoration/components/h5/config/index.tsx
@@ -3,6 +3,7 @@ import styles from "./index.module.scss";
 import { CodeSet } from "./code";
 import { VodV1Set } from "./vod-v1";
 import { LiveV1Set } from "./live-v1";
+import { BookV1Set } from "./book-v1";
 
 interface PropInterface {
   block: any;
@@ -23,6 +24,10 @@ export const ConfigSetting: React.FC<PropInterface> = ({ block, onUpdate }) => {
       {block.sign === "h5-live-v1" ||
         (block.sign === "pc-live-v1" && (
           <LiveV1Set block={block} onUpdate={() => update()}></LiveV1Set>
+        ))}
+      {block.sign === "h5-book-v1" ||
+        (block.sign === "pc-book-v1" && (
+          <BookV1Set block={block} onUpdate={() => update()}></BookV1Set>
         ))}
       {block.sign === "code" && (
         <CodeSet block={block} onUpdate={() => update()}></CodeSet>

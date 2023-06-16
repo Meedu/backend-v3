dd3035a3bec9b09e9d57388d5fde92d64d40468e
禺狨 <18119604035@163.com>
2023-06-16 11:45:58 +0800

    装修h5公众号渲染组件

--- src/pages/decoration/components/h5/render-gzh-v1/index.tsx
+++ src/pages/decoration/components/h5/render-gzh-v1/index.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import styles from "./index.module.scss";
+import noneIcon from "../../../../../assets/images/decoration/h5/none-img.png";
 
 interface PropInterface {
   config: any;
@@ -8,5 +9,24 @@ interface PropInterface {
 export const RenderGzhV1: React.FC<PropInterface> = ({ config }) => {
   const [loading, setLoading] = useState<boolean>(false);
 
-  return <div className={styles["gzh-box"]}></div>;
+  return (
+    <div className={styles["gzh-box"]}>
+      <div className={styles["gzh-left"]}>
+        <div className={styles["avatar"]}>
+          {config.logo ? (
+            <img src={config.logo} width={50} height={50} />
+          ) : (
+            <img src={noneIcon} width={50} height={50} />
+          )}
+        </div>
+        <div className={styles["info"]}>
+          <div className={styles["name"]}>{config.name}</div>
+          <div className={styles["desc"]}>{config.desc}</div>
+        </div>
+      </div>
+      <div className={styles["gzh-right"]}>
+        <div className={styles["btn"]}>关注</div>
+      </div>
+    </div>
+  );
 };
